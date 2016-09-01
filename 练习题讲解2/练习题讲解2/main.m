439adb5076944abd9ea22447f7470511a18b5d5d
DreamsSummer <779388649@qq.com>
2016-09-01 17:52:32 +0800

    对练习题的讲解

---
+++ 练习题讲解2/练习题讲解2/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  练习题讲解2
+//
+//  Created by liser on 16/3/21.
+//  Copyright © 2016年 liser. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
